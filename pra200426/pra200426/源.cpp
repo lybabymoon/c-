59724b8afbedd4b121b177ca8e33acd2c1ca5aee
lybabymoon <1468925682@qq.com>
2020-05-09 00:20:42 +0800

    ？？？

---
+++ pra200426/pra200426/源.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+using namespace std;
+//string getStr(string s)
+//{
+//	if (s.size() == 0)
+//	{
+//		return "";
+//	}
+//	string ans;
+//	stack<char> st1;
+//	for (int i = 0;i < s.size();i++)
+//	{
+//		if (s[i] == '[')
+//		{
+//			st1.push(']');
+//		}
+//		else if (s[i] == ']')
+//		{
+//			st1.pop();
+//		}
+//		else if()
+//	}
+//
+//	return ans;
+//}
+int main()
+{
+	string s;
+	cin >> s;
+	s = getStr(s);
+	cout << s << endl;
+	return 0;
+}
